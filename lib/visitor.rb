e5ca8534397bdb20c18497a9c2e975b1fd66b13d
Thomas Hawley <thawley22@turing.edu>
2023-02-27 09:51:00 -0700

    refactor: Add @spending_money to attr_accessor

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,8 +1,9 @@
 class Visitor
   attr_reader :name, 
               :height,
-              :spending_money,
               :preferences
+
+  attr_accessor :spending_money
 
   def initialize(name, height, spending_money)
     @name = name
