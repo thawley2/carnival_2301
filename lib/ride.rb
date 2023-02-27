95ed5b5258dbedd107b4cc827ee8c4279db13d03
Thomas Hawley <thawley22@turing.edu>
2023-02-27 09:29:01 -0700

    feat: Add #initialize methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,15 @@
 class Ride
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue
  
+  def initialize(ride_info)
+    @name = ride_info[:name]
+    @min_height = ride_info[:min_height]
+    @admission_fee = ride_info[:admission_fee]
+    @excitement = ride_info[:excitement]
+    @total_revenue = 0
+  end
 end
