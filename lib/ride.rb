f5f5644abcb143c2ba24fb1fb248565e478bb2cb
Thomas Hawley <thawley22@turing.edu>
2023-02-27 10:08:44 -0700

    refactor: Update #board_rider with helper methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -3,7 +3,8 @@ class Ride
               :min_height,
               :admission_fee,
               :excitement,
-              :total_revenue
+              :total_revenue,
+              :rider_log
  
   def initialize(ride_info)
     @name = ride_info[:name]
@@ -11,6 +12,7 @@ class Ride
     @admission_fee = ride_info[:admission_fee]
     @excitement = ride_info[:excitement]
     @total_revenue = 0
+    @rider_log = Hash.new(0)
   end
 
   def match_preference(visitor)
@@ -21,10 +23,15 @@ class Ride
     visitor.tall_enough?(@min_height)
   end
 
+  def charge_visitor_fee(visitor)
+    visitor.spending_money -= @admission_fee
+    @total_revenue += @admission_fee
+  end
+
   def board_rider(visitor)
     if match_preference(visitor) && meets_min_height(visitor)
-      visitor.spending_money -= @admission_fee
-      @total_revenue += @admission_fee
+      charge_visitor_fee(visitor)
+      @rider_log[visitor] += 1
     end
   end
 end
