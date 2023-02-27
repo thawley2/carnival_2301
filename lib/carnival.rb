eca8afcc5aeafe013ae480003268c453af95511b
Thomas Hawley <thawley22@turing.edu>
2023-02-27 11:01:11 -0700

    feat: Add #ride_ride method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,7 +1,8 @@
 class Carnival
-  attr_reader :event_days, :rides
+  attr_reader :name, :event_days, :rides
 
   def initialize(carnival_details)
+    @name = carnival_details[:name]
     @event_days = carnival_details[:event_days]
     @rides = []
   end
@@ -9,4 +10,17 @@ class Carnival
   def add_ride(ride)
     @rides << ride
   end
+
+  def ride_ride(visitor, ride)
+    if @rides.include?(ride)
+      ride.board_rider(visitor)
+      'Weeee'
+    else
+      'That ride does not exist here'
+    end
+  end
+
+  # def most_popular_ride
+  #   @rides.map do |ride|
+  # end
 end
